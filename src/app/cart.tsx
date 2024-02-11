7443a7a037baf7b80fa8d6c55fa836f2389251f7
David <dfl8@aluno.ifal.edu.br>
2024-02-11 12:00:19 -0300

    feat: Send order option added to cart

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -3,6 +3,11 @@ import { Header } from "@/components/header";
 import { View, Text, ScrollView } from "react-native";
 import { Products } from "@/components/products";
 import { formatCurrency } from "@/utils/functions/format-currency";
+import { Input } from "@/components/input";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Button } from "@/components/button";
+import { Feather } from "@expo/vector-icons";
+import { LinkButton } from "@/components/link-button";
 
 export default function Cart() {
     const cartStore = useCartStore();
@@ -10,23 +15,37 @@ export default function Cart() {
     return (
         <View className="flex-1 pt-8">
             <Header title="Seu carrinho" />
-            <ScrollView>
-                {cartStore.products.length > 0 ?
+            <KeyboardAwareScrollView>
+                <ScrollView>
                     <View className="p-5 flex-1">
-                        {
-                            cartStore.products.map((product) => (
-                                <Products data={product} key={product.id} />
-                            ))
+                        {cartStore.products.length > 0 ?
+                            <View className="border-b border-slate-700">
+                                {
+                                    cartStore.products.map((product) => (
+                                        <Products data={product} key={product.id} />
+                                    ))
+                                }
+                            </View>
+                            :
+                            <Text className="text-slate-400 font-body text-center my-8">Seu carrinho está vazio</Text>
                         }
+                        <View className="flex-row gap-2 items-center mt-5 mb-4">
+                            <Text className="text-white text-xl font-subtitle">Total:</Text>
+                            <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
+                        </View>
+                        <Input placeholder="Informe seu endereço"></Input>
                     </View>
-                    :
-                    <Text className="text-slate-400 font-body text-center my-8">Seu carrinho está vazio</Text>
-                }
-                <View className="flex-row gap-2 items-center mt-5 mb-4">
-                    <Text className="text-white text-xl font-subtitle">Total:</Text>
-                    <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
-                </View>
-            </ScrollView>
+                </ScrollView>
+            </KeyboardAwareScrollView>
+            <View className="p-5 gap-5">
+                <Button>
+                    <Button.Text>Enviar pedido</Button.Text>
+                    <Button.Icon>
+                        <Feather name="arrow-right-circle" size={20}/>
+                    </Button.Icon>
+                </Button>
+                <LinkButton title="Voltar ao cardápio" href="/" />
+            </View>
         </View>
     );
 }
