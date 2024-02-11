f44d646c7c7bbd641e2af32f50e2439af42ddb44
David <Dfl8@aluno.ifal.edu.br>
2024-02-10 23:35:46 -0300

    feat: CategoryButton component added

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@/components/Header";
-import { Text, View } from "react-native"
+import { CategoryButton } from "@/components/category-button";
+import { FlatList, View } from "react-native"
 
 export default function Home(){
     return (
         <View className="flex-1 pt-8">
             <Header title="Faça seu pedido" cardValue={0}/>
+            <CategoryButton title="Lanche do dia"/>
         </View>
     );
 }
