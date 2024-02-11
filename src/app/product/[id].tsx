c505cac1a02e41c5693a3a542aafe4877c0eecda
David <Dfl8@aluno.ifal.edu.br>
2024-02-11 01:09:22 -0300

    feat: formatCurrency function added

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,14 +1,21 @@
-import { View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
+
+import { PRODUCTS } from "@/utils/data/products";
+import { formatCurrency } from "@/utils/functions/format-currency";
 
 export default function Product(){
     
     const { id } = useLocalSearchParams();
+
+    const product = PRODUCTS.filter((item) => item.id === id)[0]
     
-    console.log(id);
     return(
         <View className="flex-1">
-
+            <Image source={product.cover} className="w-full h-52" resizeMode="cover"/>
+            <View className="p-5 mt-8 flex-1">
+                <Text className="text-lime-400 text-2xl font-heading my-2">{formatCurrency(product.price)}</Text>
+            </View>
         </View>
     );
 }
