a1fc2e12ad0b8a7bbd0fbe71929abf21b99c12ca
David <Dfl8@aluno.ifal.edu.br>
2024-02-11 01:27:15 -0300

    feat: Button component added

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -3,6 +3,8 @@ import { useLocalSearchParams } from "expo-router";
 
 import { PRODUCTS } from "@/utils/data/products";
 import { formatCurrency } from "@/utils/functions/format-currency";
+import { Button } from "@/components/button";
+import { Feather } from "@expo/vector-icons";
 
 export default function Product(){
     
@@ -15,6 +17,22 @@ export default function Product(){
             <Image source={product.cover} className="w-full h-52" resizeMode="cover"/>
             <View className="p-5 mt-8 flex-1">
                 <Text className="text-lime-400 text-2xl font-heading my-2">{formatCurrency(product.price)}</Text>
+                <Text className="text-slate-400 font-body text-base leading-6 mb-6">{product.description}</Text>
+                {
+                    product.ingredients.map((ingredient) => (
+                        <Text className="text-slate-400 font-body text-base leading-6" key={ingredient}>{"\u2022 "}{ingredient}</Text>
+                    ))
+                }
+            </View>
+            <View className="p-5 pb-8 gap-5">
+                <Button>
+                    <Button.Icon>
+                        <Feather name="plus-circle" size={20}/>
+                    </Button.Icon>
+                    <Button.Text>
+                        Adicionar ao pedido
+                    </Button.Text>
+                </Button>
             </View>
         </View>
     );
