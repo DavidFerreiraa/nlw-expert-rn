4a6928b4965059a11d7d6c594adaf25997902c12
David <dfl8@aluno.ifal.edu.br>
2024-02-11 10:54:50 -0300

    feat: ScrollView and total added to cart

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,20 +1,32 @@
 import { useCartStore } from "@/stores/cart-stores";
 import { Header } from "@/components/header";
-import { View } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { Products } from "@/components/products";
+import { formatCurrency } from "@/utils/functions/format-currency";
 
-export default function Cart(){
+export default function Cart() {
     const cartStore = useCartStore();
-    return(
+    const total = formatCurrency(cartStore.products.reduce((total, products) => total + products.price * products.quantity, 0));
+    return (
         <View className="flex-1 pt-8">
-            <Header title="Seu carrinho"/>
-            <View className="p-5 flex-1">
-                {
-                    cartStore.products.map((product) => (
-                        <Products data={product} key={product.id} />
-                    ))
+            <Header title="Seu carrinho" />
+            <ScrollView>
+                {cartStore.products.length > 0 ?
+                    <View className="p-5 flex-1">
+                        {
+                            cartStore.products.map((product) => (
+                                <Products data={product} key={product.id} />
+                            ))
+                        }
+                    </View>
+                    :
+                    <Text className="text-slate-400 font-body text-center my-8">Seu carrinho está vazio</Text>
                 }
-            </View>
+                <View className="flex-row gap-2 items-center mt-5 mb-4">
+                    <Text className="text-white text-xl font-subtitle">Total:</Text>
+                    <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
+                </View>
+            </ScrollView>
         </View>
     );
 }
